73e683d45a9b7da19756cc7614a450e46eb21b48
Ayça Aksoy <aycaaksoy2000@gmail.com>
2023-01-05 12:50:04 +0300

    movies list api-consume

---
+++ WebAPIConsume/UP_School_API_Consume/Controllers/MovieController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using UP_School_API_Consume.Models;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace UP_School_API_Consume.Controllers
+{
+    public class MovieController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<MovieListViewModel> movies = new List<MovieListViewModel>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "1fabfe500dmsh7a46774fc75b1bep10dcb7jsn16046b3e6e14" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                movies = JsonConvert.DeserializeObject<List<MovieListViewModel>>(body);
+                return View(movies);
+            }
+           
+        }
+    }
+}
