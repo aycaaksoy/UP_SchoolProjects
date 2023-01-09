cd6a2981fcd584ccff99c15274edaed7d6583aa0
Ayça Aksoy <aycaaksoy2000@gmail.com>
2023-01-09 16:36:14 +0300

    signalR

---
+++ UpSchool_SignalR_Api2/UpSchool_SignalR_Api2/Hubs/ElectricService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UpSchool_SignalR_Api2.Models;
+
+namespace UpSchool_SignalR_Api2.Hubs
+{
+    public class ElectricService
+    {
+        private readonly Context _context;
+        private readonly IHubContext<ElectricHub> _hubContext;
+        public ElectricService(Context context, IHubContext<ElectricHub> hubContext)
+        {
+            _context = context;
+            _hubContext = hubContext;
+        }
+
+        public IQueryable<Electric> GetList()
+        {
+            return _context.Electrics.AsQueryable();
+        }
+
+        public async Task SaveElectric(Electric electric)
+        {
+            await _context.Electrics.AddAsync(electric);
+            await _context.SaveChangesAsync();
+            await _hubContext.Clients.All.SendAsync("ReceiveElectricList", "data");
+        }
+    }
+}
